fc670437e19b4172479bb0317c818e3751b0d5c4
Katie Blackstone <katierblackstone@gmail.com>
2021-07-28 19:31:08 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-	return <div className='App'></div>;
+	const [buttonText, setButtonText] = useState('Click Me');
+	const [numTimesClicked, setNumTimesClicked] = useState(0);
+
+	return (
+		<div className='App'>
+			<h1>State Exercises</h1>
+			<button onClick={() => setNumTimesClicked(numTimesClicked + 1)}>
+				{buttonText}
+			</button>
+			<input
+				type='text'
+				placeholder='Change button text'
+				onChange={event => setButtonText(event.target.value)}
+			></input>
+			<div className='stateDisplay'>
+				<h2>State Values</h2>
+				<p>Button Text: {buttonText}</p>
+				<p>Number of Times Clicked: {numTimesClicked}</p>
+			</div>
+		</div>
+	);
 }
 
 export default App;
